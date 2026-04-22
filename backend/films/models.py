4367fffcf7cbc0ee20d69784e3b701f155838f10
Julien Aldon <julien2.aldon@epitech.eu>
2026-04-22 11:01:32 +0200

    add(backend): add first django and djangorestframework template

---
+++ backend/films/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Actor(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField()
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    actors = models.ForeignKey(Actor, on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    grade = models.FloatField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
